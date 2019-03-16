7cc72f069a163e56e63f0d05dbb8d04abdf9a59a
Tom Vincent <git@tlvince.com>
2019-03-16 19:15:31 +0000

    fix: ignore empty lines and comments

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-mkdir -p "build"
+rm -rf build && mkdir -p "build"
 
 src="https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn-social/hosts"
-curl "$src" >> "build/hosts"
+curl "$src" > "build/hosts"
 
 while read -r line; do
   sed -i "/$line$/d" "build/hosts"
-done < "whitelist.txt"
+done < <(grep -v -e '^[[:space:]]*$' -e '^#' "whitelist.txt")
 
 cat "blacklist.txt" >> "build/hosts"
